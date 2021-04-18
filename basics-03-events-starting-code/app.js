7144e9734e8db164d352d3a0664a7c4ee7cac947
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-18 11:14:41 +0900

    Event Modifiers 적용 (.prevent, .right, .enter ,,) 및 v-once 를 이용한 처음 상태 보존

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,11 +1,18 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
       name: '',
+      confirmedName: '',
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted');
+    },
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName;
     },
