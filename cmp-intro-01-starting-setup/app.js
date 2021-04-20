94bc3ccc4495cbc93fc9ca2a6dafd4bf2a2dad9a
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-21 02:35:04 +0900

    각각의 독립된 요소로 관리할 수 있는 Component 이용

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: true,
       friends: [
         { 
           id: 'manual', 
@@ -18,10 +17,34 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component('friend-contact', { // component 이름은 두 단어 이상으로 구분자는 '-' 로 짓는 게 일반적. (HTML 태그가 한 단어이기 떄문)
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: true,
+      friend: { 
+        id: 'manual', 
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
-    }
+    },
   },
 });
 
