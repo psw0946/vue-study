8005cdeaf5322d285a55fbe9c43fe08a4bd113d2
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-22 00:33:35 +0900

    Introduce Props : Parent => Child Communication

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,16 +1,21 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }}</h2>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong> {{ friend.phone }}</li>
-      <li><strong>Email:</strong> {{ friend.email }}</li>
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
+  props: [
+    'name',
+    'phoneNumber', // kebab case 로 작성된 props 를 vue 가 camelCase 로 알아서 바꿔줌.
+    'emailAddress'
+  ],
   data() {
     return {
       detailsAreVisible: false,
