30ff6338fe19e250508039fa10c39090a114011b
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-25 15:42:23 +0900

    Handling Loading & No Data Situation

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -5,7 +5,9 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some survey results first.</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -27,15 +29,18 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch('https://vue-demo-app-ed2da-default-rtdb.firebaseio.com/surveys.json').then(function(response) {
         if (response.ok) {
           return response.json();
         }
       }).then((data) => {
+        this.isLoading = false;
         const results = [];
         for (const id in data) {
           results.push({ id: id, name: data[id].name, rating: data[id].rating });
