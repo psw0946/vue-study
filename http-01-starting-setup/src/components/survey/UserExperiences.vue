8d09c8ca7b652d50378a60a2e64f8cd6cef5f559
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-25 15:34:49 +0900

    Get/Save Data with Firebase Backend

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -21,9 +21,31 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://vue-demo-app-ed2da-default-rtdb.firebaseio.com/surveys.json').then(function(response) {
+        if (response.ok) {
+          return response.json();
+        }
+      }).then((data) => {
+        const results = [];
+        for (const id in data) {
+          results.push({ id: id, name: data[id].name, rating: data[id].rating });
+        }
+        this.results = results;
+      });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
