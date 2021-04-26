633b71e180c1dcea5860c3319690d42c94d2722b
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-26 23:20:47 +0900

    Rendering Multiple Routes with Named Router Views
    name 이 없는 것은 기본적으로 default 에 해당함.

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,12 +16,12 @@ const router = createRouter({
     { 
       name: 'temas',
       path: '/teams',
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter }, // router-view name 이 없는 것은 default 로 들어가고, footer 키 컴포넌트는 name 이 footer 인 곳으로 들어감.
       children: [
         { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
       ],
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
