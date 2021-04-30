54d6877d14d86836c4b4f6f63c3cfe463fc5956a
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-30 20:38:01 +0900

    Change default name of vue provided animation class

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -4,7 +4,7 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -92,25 +92,25 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from {
+.para-enter-from {
   /* opacity: 0;
   transform: translateY(-30px); */
 }
-.v-enter-active {
+.para-enter-active {
   animation: slide-scale 0.3s ease-out;
 }
-.v-enter-to {
+.para-enter-to {
   /* opacity: 1;
   transform: translateY(0); */
 }
-.v-leave-from {
+.para-leave-from {
   /* opacity: 1;
   transform: translateY(0); */
 }
-.v-leave-active {
+.para-leave-active {
   animation: slide-scale 0.3s ease-out;
 }
-.v-leave-to {
+.para-leave-to {
   /* opacity: 0;
   transform: translateY(30px); */
 }
