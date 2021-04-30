dd39d8845585cfb70c4b661393d89143273ccaea
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-30 22:03:38 +0900

    Animating List Item Movement - transition-group
    - 한 아이템을 애니메이션 조작할 때: transition, 여러 아이템: transition-group
    - transition-group 사용 시 한 아이템의 애니메이션 발동으로 인한 다른 아이템들 이동 시 .v-move 클래스가 붙어서 제어 가능

--- animations-01-starting-setup/src/components/UsersList.vue
+++ animations-01-starting-setup/src/components/UsersList.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul>
+  <transition-group tag="ul" name="user-list">
     <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-  </ul>
+  </transition-group>
   <div>
     <input type="text" ref="userNameInput">
     <button @click="addUser">Add User</button>
@@ -38,5 +38,30 @@ li {
   border: 1px solid #ccc;
   padding: 1rem;
   text-align: center;
+}
+
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.user-list-enter-active {
+  transition: all 1s ease-out;
+}
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0px);
+}
+.user-list-leave-active {
+  transition: all 1s ease-in;
+  position: absolute; /* 중간 것이 제거되면서 화면이 순간적으로 팍 흔들리는 현상 제거를 위함. */
+}
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.user-list-move { /* 하나가 삭제되거나 삽입될 때 나머지 것들에 붙음. */
+  transition: transform 0.8s ease;
 }
 </style>
