b6d4c90d11e09f99c30f7a1438a673fd435f210c
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-04-30 22:30:50 +0900

    Animating Route Changes
    - router-view 안에 transition 태그를 넣고 컴포넌트를 렌더링 한다.
    - component 는 root element 가 하나여야 한다.

--- animations-01-starting-setup/src/main.js
+++ animations-01-starting-setup/src/main.js
@@ -1,10 +1,28 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 import BaseModal from './components/BaseModal.vue';
+import AllUsers from './pages/AllUsers.vue';
+import CourseGoals from './pages/CourseGoals.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: AllUsers },
+    { path: '/goals', component: CourseGoals },
+  ],
+});
 
 const app = createApp(App);
 
+app.use(router);
+
 app.component('base-modal', BaseModal);
 
-app.mount('#app');
+// router 는 기본적으로 처음 로딩 시 empty route 이었다가 url 파악 후 전환된다.
+// 이로 인해 첫 로딩 시에도 애니메이션이 발동.
+// 첫 로딩 시 url 파악 후 로직을 수행하도록 isReady 를 사용할 수 있다.
+router.isReady().then(function() {
+  app.mount('#app');
+});
