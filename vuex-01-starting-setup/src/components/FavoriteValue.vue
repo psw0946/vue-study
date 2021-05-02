554b55321e8bde9c0f230c2ce2b1f1549f4a6695
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-05-02 14:09:17 +0900

    Using vuex getters - get and set state
    - 다른 getter 를 받아올 수 있다.

--- vuex-01-starting-setup/src/components/FavoriteValue.vue
+++ vuex-01-starting-setup/src/components/FavoriteValue.vue
@@ -1,15 +1,16 @@
 <template>
   <h3>{{ counter }}</h3>
+  <p>We do more..</p>
 </template>
 
 <script>
 export default {
-  name: 'TheCounter',
+  name: 'FavoriteValue',
   computed: {
     counter() {
-      return this.$store.getters.finalCounter;
+      return this.$store.getters.normalizedCounter;
     },
-  },
+  }
 };
 </script>
 
