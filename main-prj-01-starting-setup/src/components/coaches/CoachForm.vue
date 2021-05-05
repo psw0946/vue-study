e71bc60167ee3b889e8c690294f91074a07138c7
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-05-05 14:11:29 +0900

    Coach Registration to Vuex

--- main-prj-01-starting-setup/src/components/coaches/CoachForm.vue
+++ main-prj-01-starting-setup/src/components/coaches/CoachForm.vue
@@ -38,6 +38,7 @@
 <script>
 export default {
   name: 'CoachForm',
+  emits: ['save-data'],
   data() {
     return {
       firstName: '',
@@ -57,7 +58,7 @@ export default {
         areas: this.areas,
       };
 
-      console.log(formData);
+      this.$emit('save-data', formData);
     },
   },
 };
