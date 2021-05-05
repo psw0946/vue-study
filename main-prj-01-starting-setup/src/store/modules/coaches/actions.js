eeef1a5516b993b5823bc13c00b2a3ab2f5aeb02
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-05-05 16:50:48 +0900

    Adding Http Error Handling

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -28,8 +28,10 @@ export default {
   async loadCoaches(context) {
     const response = await fetch(`https://vue-demo-app-ed2da-default-rtdb.firebaseio.com/coaches.json`);
     const responseData = await response.json();
+
     if (!response.ok) {
-      // ...
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
     }
 
     const coaches = [];
