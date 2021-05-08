587327314d23e4bb48926e16c11f786f85adbefc
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-05-08 20:37:36 +0900

    Adding Auto Login with Local Storage

--- main-prj-01-starting-setup/src/App.vue
+++ main-prj-01-starting-setup/src/App.vue
@@ -15,6 +15,9 @@ export default {
   components: {
     TheHeader,
   },
+  created() {
+    this.$store.dispatch('tryLogin');
+  }
 };
 </script>
 
--- main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,6 +1,23 @@
 export default {
   async login(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB3uJTZu87pvlvY2HKyXOcqFdtorbUi3hw', {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB3uJTZu87pvlvY2HKyXOcqFdtorbUi3hw';
+    if (mode === 'signup') {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB3uJTZu87pvlvY2HKyXOcqFdtorbUi3hw';
+    }
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         email: payload.email,
@@ -12,42 +29,30 @@ export default {
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn,
     });
   },
-  async signup(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB3uJTZu87pvlvY2HKyXOcqFdtorbUi3hw', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
-      throw error;
+    if (token && userId) {
+      context.commit('setUser', {
+        token,
+        userId,
+        tokenExpiration: null,
+      })
     }
-
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
   },
   logout(context) {
     context.commit('setUser', {
