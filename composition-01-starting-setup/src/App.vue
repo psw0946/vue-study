60863db7c62ba953484bda19c0fded8ea6c2a2c5
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-05-09 20:21:18 +0900

    Replacing data() with setup() and ref()
    - ref 를 사용하면 vue 가 관리하는 object 를 반환.
    - value 프로퍼티를 사용하여 변경 감지 가능.
    - template 단에서 value 프로퍼티를 안 써도 vue 가 알아서 사용해줌

--- composition-01-starting-setup/src/App.vue
+++ composition-01-starting-setup/src/App.vue
@@ -5,12 +5,23 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
-    };
+  setup() {
+    const uName = ref('Maximilian');
+
+    setTimeout(function() {
+      uName.value = 'Max';
+    }, 2000);
+
+    return { userName: uName };
   },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
 };
 </script>
 
