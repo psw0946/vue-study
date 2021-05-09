56b307d31c90f967ab0dceb8970505e9c9645fbe
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-05-09 20:09:08 +0900

    Lazy Loading Unused Components
    - 일종의 최적화: 한 번에 모든 컴포넌트를 로드하여 번들링하는데,
    - 라우터에서 안 쓰는 컴포넌트들은 나중에 쓰일 때 로드하도록 변경 가능
    - 그런데, defineAsyncComponent 는 쓰지 말라고 강의 다음 문서에 써있음 ;;

--- main-prj-01-starting-setup/src/main.js
+++ main-prj-01-starting-setup/src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import router from '@/router';
 import store from '@/store';
@@ -7,7 +7,9 @@ import BaseCard from '@/components/ui/BaseCard';
 import BaseButton from '@/components/ui/BaseButton';
 import BaseBadge from '@/components/ui/BaseBadge';
 import BaseSpinner from '@/components/ui/BaseSpinner';
-import BaseDialog from '@/components/ui/BaseDialog';
+// import BaseDialog from '@/components/ui/BaseDialog';
+
+const BaseDialog = defineAsyncComponent(() => import('@/components/ui/BaseDialog'));
 
 const app = createApp(App);
 
--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -1,13 +1,20 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from '@/pages/coaches/CoachDetail';
+// import CoachDetail from '@/pages/coaches/CoachDetail';
 import CoachesList from '@/pages/coaches/CoachesList';
-import CoachRegistration from '@/pages/coaches/CoachRegistration';
-import ContactCoach from '@/pages/requests/ContactCoach';
-import RequestsReceived from '@/pages/requests/RequestsReceived';
+// import CoachRegistration from '@/pages/coaches/CoachRegistration';
+// import ContactCoach from '@/pages/requests/ContactCoach';
+// import RequestsReceived from '@/pages/requests/RequestsReceived';
 import NotFound from '@/pages/NotFound';
-import UserAuth from '@/pages/auth/UserAuth';
+// import UserAuth from '@/pages/auth/UserAuth';
 import store from '@/store';
+
+const CoachDetail = defineAsyncComponent(() => import('@/pages/coaches/CoachDetail'));
+const CoachRegistration = defineAsyncComponent(() => import('@/pages/coaches/CoachRegistration'));
+const ContactCoach = defineAsyncComponent(() => import('@/pages/requests/ContactCoach'));
+const RequestsReceived = defineAsyncComponent(() => import('@/pages/requests/RequestsReceived'));
+const UserAuth = defineAsyncComponent(() => import('@/pages/auth/UserAuth'));
 
 const router = createRouter({
   history: createWebHistory(),
