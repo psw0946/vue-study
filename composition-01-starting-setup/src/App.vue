f029eb4e26970f48dad253c0afa6d0032c2fb4e8
coma.park <coma.park@comaparkui-MacBookPro.local>
2021-05-10 00:33:13 +0900

    Building 'reactive' Objects
    - object 를 ref 로 감싸서 value 를 수정하면 갱신되지 않음.
    - reactive 로 감싸주면 갱신됨. (value property 를 확인하지 않고 바로 접근 가능)

--- composition-01-starting-setup/src/App.vue
+++ composition-01-starting-setup/src/App.vue
@@ -1,21 +1,30 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
 export default {
   setup() {
-    const uName = ref('Maximilian');
+    // const uName = ref('Maximilian');
+    // const uAge = ref(31);
+    const user = reactive({
+      name: 'Maximilian',
+      age: 31,
+    });
 
     setTimeout(function() {
-      uName.value = 'Max';
+      // uName.value = 'Max';
+      // uAge.value = 32;
+      user.name = 'Max';
+      user.age = 32;
     }, 2000);
 
-    return { userName: uName };
+    return { user: user };
   },
   // data() {
   //   return {
